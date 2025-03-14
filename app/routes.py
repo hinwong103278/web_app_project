8b56f2b012310176f33ece1d51c8fdf41264fb2a
hinwong103278 <wonghin854@gmail.com>
2025-03-14 17:55:19 +0000

    123

--- app/routes.py
+++ app/routes.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask import render_template, redirect, flash, url_for, request
 from flask_login import login_required, current_user, login_user, logout_user
 from urllib.parse import urlparse
+
 from app import app, db
 from app.forms import LoginForm, RegistrationForm, EditProfileForm
 from app.models import User
@@ -87,7 +88,7 @@ def user(username):
 @app.route('/edit_profile', methods=['GET', 'POST'])
 @login_required
 def edit_profile():
-    form = EditProfileForm()
+    form = EditProfileForm(current_user.username)
     if form.validate_on_submit():
         current_user.username = form.username.data
         current_user.about_me = form.about_me.data
