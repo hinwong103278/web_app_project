382c6de8f461e1aae9341323755550fde3e45d4b
hinwong103278 <wonghin854@gmail.com>
2025-03-19 12:26:30 +0000

    lab10

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get("SECRET_KEY") or "you-will-never-guess"
     SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI") or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'postgresql://postgres:postgres@postgresdb:5432/postgres'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = os.environ.get('MAIL_SERVER') or "mailhog"
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 1025)
@@ -14,3 +14,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['cywong@example.com']
+    POSTS_PER_PAGE = 3
