53129b447463ef92394bfefb20ca052d8bb08a9e
hinwong103278 <wonghin854@gmail.com>
2025-03-19 13:11:46 +0000

    lab13

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,15 @@
 import logging
 from logging.handlers import RotatingFileHandler, SMTPHandler
 import os
-from flask import Flask
+from flask import Flask, request
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_babel import Babel
 
 
 app = Flask(__name__)
@@ -15,6 +19,10 @@ migrate = Migrate(app, db)
 login = LoginManager()
 login.login_view = "login"
 login.init_app(app)
+mail = Mail(app)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+babel = Babel(app)
 
 if not app.debug:
     root = logging.getLogger()
@@ -44,6 +52,9 @@ if not app.debug:
     root.setLevel(logging.INFO)
     root.info('Microblog startup')
 
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 # You must keep the routes at the end.
 from app import routes, errors
