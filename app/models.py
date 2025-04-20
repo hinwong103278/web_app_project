38d924f8c4aad8e8f1e978460ef9c17b0b0ceb10
oscar12802580 <longlenghuang@gmail.com>
2025-04-20 10:16:49 +0000

    update index&base

--- app/models.py
+++ app/models.py
@@ -87,6 +87,8 @@ def load_user(id):
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.String(140))
+    title = db.Column(db.String(100))
+    content = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
